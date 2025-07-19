900d12082db4d29f2d6a8bb65c09ce9faf736a6a
M-GreySun <mgreysun@gmail.com>
2025-07-19 00:37:05 -0400

    Completed some simple exercises. I don't care for the capitalization methond as of now, since right now it's not handling while/ blank space and escape characters the way I would like it to do so.

--- playground/functions.js
+++ playground/functions.js
@@ -11,10 +11,6 @@
  * Write a function called lastLetter that takes a string and returns the very last letter of that string:
  * lastLetter("abcd") should return "d"
  * 
- * 
- * 
- * 
- *
  */
 
 function add7(num){
@@ -24,6 +20,58 @@ function multiply(num1, num2){
     return num1 * num2;
 }
 
+function capitalize(word){
+    if (typeof word != "string"){
+        return "Invalid Input";
+    }
+    if( word.length==0){
+        return "The string is empty: " + word;
+    }
+
+    if(word.length==1){
+        return word.toUpperCase();
+    }
+    return word.substring(0,1).toUpperCase() + word.substring(1, word.legnth).toLowerCase();
+}
+function cap(word){
+    if (typeof word != "string"){
+        return "Invalid Input";
+    }
+    if( word.length==0){
+        return "The string is empty: " + word;
+    }
+    let i;
+    for (i=0; i<word.length; i++){
+        if(typeof word.at(i)== "string"){
+            return word.substring(0, i).toUpperCase() + word.substring(i, word.length).toLowerCase(); 
+        }
+    }
+    return "The end of the function";
+}
+
+function lastLetter(word){
+    return word.at(word.length-1);
+}
+
 console.log(add7(1));
 
 console.log(multiply(3, 9));
+
+let value= "the below Answer Should have only ONE capital letter IN it."
+console.log(value);
+console.log(capitalize(value));
+
+value="        ";
+console.log("The below string are blank");
+console.log(value);
+console.log(capitalize(value));
+
+value= " 434";
+console.log(value);
+console.log(capitalize(value));
+
+let val= "     4353 IN this pharse the first characters are blank spaces and numbers";
+console.log("First imiplementation: "+ capitalize(val));
+
+val= lastLetter("abcd");
+console.log(val);

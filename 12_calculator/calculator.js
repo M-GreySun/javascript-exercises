56dc3a53f7832c98a7451a7384be3ab23d0f86c5
M-GreySun <mgreysun@gmail.com>
2025-07-25 21:13:14 -0400

    On the caluculator problem. This the 12 exercise. I'm not sure why my functon is returning an undefined value. It works find when I run in from the console, however, it dose not pass the unit test. I played around with the forEach method and the reduce method for array. Both gave the same results.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,6 +1,25 @@
 const add = function() {
-	
+
+  let sum=0; 
+  Array.from(arguments).forEach((ele)=>{  //arguments must be converted to an array first.
+    if( typeof ele=== "number"){                //Arguments in an array like object available in functions.
+      sum+= ele;                                //For Each do not return a value.
+    }
+    else{
+      sum+=ele.length>0? ele.reduce((total, current)=>{return total+current}): 0;
+    }
+  });
+  console.log("The date type of sum: "+ typeof sum);
+  console.log("The value of sum: "+ sum);
+  return sum;
 };
+let value= add([]);
+console.log(typeof value);
+value= add([7,11]);
+console.log(typeof value);
+value =add([7]);
+console.log(typeof value);
+value= add([1, 3, 5, 7, 9])
 
 const subtract = function() {
 	

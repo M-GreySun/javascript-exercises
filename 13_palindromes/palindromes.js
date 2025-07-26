c5f1eb407e6e4a31969fbe0c71862277c4ccf71c
M-GreySun <mgreysun@gmail.com>
2025-07-26 00:11:20 -0400

    The first solution I thought of used regex - regular expression. It's not something I use often. I did look at the given solution and the given solution filters the string to include only alpha numeric values and then does a string comparision.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    
+    let bot=0; 
+    let top= string.length-1;
+    string=string.toLowerCase();
+    
+    while(bot<top){
+        if(/[a-zA-A0-9]/.test(string[bot])&&
+                /[a-zA-A0-9]/.test(string[top]) && 
+                string[bot]!==string[top]){
+            return false;
+        }
+        else if (!/[a-zA-A0-9]/.test(string[bot])){
+            bot++;
+        }
+        else if(!/[a-zA-A0-9]/.test(string[top])){
+            top--;
+        }
+        else{
+            bot++;
+            top--;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line

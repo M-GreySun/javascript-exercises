bc3ff29838fe0323df1f95ed7aa6761abc39f3b9
M-GreySun <mgreysun@gmail.com>
2025-07-25 21:50:02 -0400

    I made some changes to the add function. I attempted the problem just just good or standard for loops, however this new implementation is failing every test. I'm going to take a look at the solution file because clearly there's something going on that I'm just not aware of.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,45 @@
 const add = function() {
 
   let sum=0; 
+/*
   Array.from(arguments).forEach((ele)=>{  //arguments must be converted to an array first.
     if( typeof ele=== "number"){                //Arguments in an array like object available in functions.
-      sum+= ele;                                //For Each do not return a value.
+      sum+= ele;                                //forEach always returns undefined
     }
     else{
-      sum+=ele.length>0? ele.reduce((total, current)=>{return total+current}): 0;
+      sum+=ele.length>0? ele.reduce((total, current)=>total+current): 0;
+      
+      //ele.forEach((item)=>{sum+=Number.parseFloat(item);}); //Must converts the array elements to numbers first
+    
     }
   });
+*/
+
+  for(let i=0; i<arguments.length; i++){
+    if(arguments[i]=== "number"){
+      //console.log("This is a number: "+ arguments[i]);
+      sum+=arguments[i];
+    }
+    else{
+      let temp=arguments[i];
+        for(let j=0; j< temp.length; j++){
+          //console.log("This is an array value: "+ Number.parseFloat(temp[j]));
+          sum+=Number.parseFloat(temp[j]);
+        }
+    }
+  }
   console.log("The date type of sum: "+ typeof sum);
   console.log("The value of sum: "+ sum);
   return sum;
 };
 let value= add([]);
-console.log(typeof value);
+console.log(value);
 value= add([7,11]);
-console.log(typeof value);
+console.log(value);
 value =add([7]);
-console.log(typeof value);
-value= add([1, 3, 5, 7, 9])
+console.log(value);
+value= add([1, 3, 5, 7, 9]);
+console.log(value);
 
 const subtract = function() {
 	

54f978ab8eb8a9dd2a627b5912448a5d188c0edb
M-GreySun <mgreysun@gmail.com>
2025-07-26 00:50:40 -0400

    Recursive implementation of the fibonacci squence.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(term) {
+    if(term<0){
+        return "OOPS";
+    }
+    else if(term<=1){
+        return Number.parseInt(term);
+    }
+    return fibonacci(term-1)+ fibonacci(term-2);
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;

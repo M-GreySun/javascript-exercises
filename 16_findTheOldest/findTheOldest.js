23241742446d56908f9df1452c007579f1e828ae
M-GreySun <mgreysun@gmail.com>
2025-07-26 02:21:03 -0400

    I'm trying several different things. I didn't really do the prerequist stuff. In the JavaScript30 Repository so, I'm pretty much pulling at straws, guessing, and googling to see how this works in javascript. I'm come back this later with fresh eyes.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let max=people.map((ele)=> ele.yearOfDeath-ele.yearOfBirth);
+    console.log(max);
+
+    max= max.reduce((num1, num2)=> Math.max(num1, num2));
+    console.log(max)
+
+    let oldest=people.filter((ele)=>ele.yearOfDeath-ele.yearOfBirth== max);
+    console.log(oldest);
+    console.log(oldest.name);
+    console.log(oldest[0]);
+    console.log(oldest[0].name);
+    return oldest[0].name;
+    //return oldest[0].name;
 };
 
+let people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ];
+
+    console.log("Who is the oldest: "+ findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;

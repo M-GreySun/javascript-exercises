836ca9fac63051ae7ec0e3c4e6633fda65198a03
M-GreySun <mgreysun@gmail.com>
2025-07-26 01:45:21 -0400

    Getting a small introduction in maps, I have an affinty for using the forEach method over filter, map, and reduce. I believe this is because the forEach method just feels like shorthand for a standard for loop.

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,27 @@
-const getTheTitles = function() {
+const getTheTitles = function(map) {
+/*
+    // My implementation
+    let array=[];
+    map.forEach((ele)=>{
+        array.push(ele.title);
+    })
 
+    return array;
+*/
+    return map.map((ele) => ele.title);         //Takes every element in the array and maps it to its title
 };
+
+const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;

9f0caa8d93e2f2b904ab49c2f2da44915760836f
M-GreySun <mgreysun@gmail.com>
2025-07-27 17:03:11 -0400

    Completed the javascript exercises. This last problem was challenging for me. I didn't know how I wanted to approach this problem. Using map and filter just seems like intermeddiate steps. I will say that I do not like that I altered my data to compare the ages of the people.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,21 +1,16 @@
 const findTheOldest = function(people) {
-
-    let max=people.map((ele)=> ele.yearOfDeath-ele.yearOfBirth);
-    console.log(max);
-
-    max= max.reduce((num1, num2)=> Math.max(num1, num2));
-    console.log(max)
-
-    let oldest=people.filter((ele)=>ele.yearOfDeath-ele.yearOfBirth== max);
-    console.log(oldest);
-    console.log(oldest.name);
-    console.log(oldest[0]);
-    console.log(oldest[0].name);
-    return oldest[0].name;
-    //return oldest[0].name;
+  let year= new Date().getFullYear();
+  people.forEach((ele) => {
+        ele['age']= !ele.yearOfDeath? year-ele.yearOfBirth: ele.yearOfDeath-ele.yearOfBirth;
+      });
+  console.log(people);
+  let oldest= people.reduce((a, b)=> a.age> b.age? a: b);
+  
+  return oldest;
 };
 
-let people = [
+
+let one = [
       {
         name: "Carly",
         yearOfBirth: 1942,
@@ -33,6 +28,43 @@ let people = [
       },
     ];
 
-    console.log("Who is the oldest: "+ findTheOldest(people));
+let two = [
+      {
+        name: "Carly",
+        yearOfBirth: 2018,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ];
+
+    let three = [
+      {
+        name: "Carly",
+        yearOfBirth: 1066,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ];
+
+  console.log("Who is the oldest: "+ findTheOldest(one).name);
+  console.log("Who is the oldest: "+ findTheOldest(two).name);
+  console.log("Who is the oldest: "+ findTheOldest(three).name);
+  
 // Do not edit below this line
 module.exports = findTheOldest;
